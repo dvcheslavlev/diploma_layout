c98a50315cb6bf7402fbb20159b713c034f75cfc
dvcheslavlev <dvcheslavlev@gmail.com>
2020-04-13 22:54:00 +0300

    scroll_off

--- script/script.js
+++ script/script.js
@@ -27,6 +27,51 @@ $('.owl-carousel').owlCarousel({
 $(function(){
     const mobMenuBtn = $('.headbar__menu-btn_mobile');
     const recallBtn = $('.recall_btn');
+    // left: 37, up: 38, right: 39, down: 40,
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    const keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+    function preventDefault(e) {
+      e = e || window.event;
+      if (e.preventDefault)
+          e.preventDefault();
+      e.returnValue = false;  
+    }
+
+    function preventDefaultForScrollKeys(e) {
+        if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+        }
+    }
+
+    function disableScroll() {
+      if (window.addEventListener) // older FF
+          window.addEventListener('DOMMouseScroll', preventDefault, false);
+
+      console.log('modernizr', Modernizr.passiveeventlisteners);
+
+      window.addEventListener('wheel', preventDefault,
+          Modernizr.passiveeventlisteners ? {passive: false} : false); // modern standard
+      document.addEventListener('mousewheel', preventDefault,
+          Modernizr.passiveeventlisteners ? {passive: false} : false); // older browsers, IE
+      window.addEventListener('mousewheel', preventDefault,
+          Modernizr.passiveeventlisteners ? {passive: false} : false); // older browsers, IE
+      window.addEventListener('touchmove', preventDefault,
+          Modernizr.passiveeventlisteners ? {passive: false} : false); // mobile
+      document.onkeydown  = preventDefaultForScrollKeys;
+    }
+
+    function enableScroll() {
+        if (window.removeEventListener)
+            window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        document.removeEventListener('mousewheel', preventDefault, false);
+        window.removeEventListener('mousewheel', preventDefault, false);
+        window.removeEventListener('wheel', preventDefault, false);
+        window.removeEventListener('touchmove', preventDefault, false);
+        document.onkeydown = null;
+    }
+
 
     mobMenuBtn.click(function(){
         $('.popup__container').fadeIn(600);
@@ -38,7 +83,7 @@ $(function(){
     recallBtn.click(function(){
         $('.popup__container').css('background', 'linear-gradient(0deg, rgba(242, 101, 34, 0.9), rgba(250, 8, 145, 0.05))');
         $('.recall-form__container').css('display', 'flex');
-        $('.popup__container').fadeIn(600);
+        $('.popup__container').fadeIn(600, disableScroll);
         $('.recall-form__container').fadeIn(300);
     });
 
@@ -52,7 +97,7 @@ $(function(){
             $('#client-name').val('');
             $('#client-email').val('');
             $('#client-phone-number').val('');
-            $('.recall-form__container').fadeOut(200);
+            $('.recall-form__container').fadeOut(200, enableScroll);
         }
     });
 });
